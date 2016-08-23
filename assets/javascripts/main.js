82e0a4d5d8789a69b0f441b70dc398ba7a327b52
Osny Netto <netto@osny.me>
2016-08-23 09:29:39 -0300

    This is where it all begins... :fire:

---
+++ assets/javascripts/main.js
@@ -0,0 +1,26 @@
+function animate(elem,style,unit,from,to,time,prop) {
+  if( !elem) return;
+  var start = new Date().getTime(),
+      timer = setInterval(function() {
+          var step = Math.min(1,(new Date().getTime()-start)/time);
+          if (prop) {
+              elem[style] = (from+step*(to-from))+unit;
+          } else {
+              elem.style[style] = (from+step*(to-from))+unit;
+          }
+          if( step == 1) clearInterval(timer);
+      },25);
+  elem.style[style] = from+unit;
+}
+
+var el = document.querySelector(".contactBtn");
+var target = document.querySelector(".footer-box");
+
+el.addEventListener("click", function(){
+  animate(document.body, "scrollTop", "", 0, target.offsetTop, 500, true);
+});
+
+// window.onload = function () {
+//   var target = document.getElementById("div5");
+//   animate(document.body, "scrollTop", "", 0, target.offsetTop, 2000, true);
+// };
