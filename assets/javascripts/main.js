ef76167b149ba6ded126e8c064e770c9147798d5
Osny Netto <netto@osny.me>
2016-08-25 21:31:39 -0300

    Remove comment code

--- assets/javascripts/main.js
+++ assets/javascripts/main.js
@@ -2,13 +2,13 @@ function animate(elem,style,unit,from,to,time,prop) {
   if( !elem) return;
   var start = new Date().getTime(),
       timer = setInterval(function() {
-          var step = Math.min(1,(new Date().getTime()-start)/time);
-          if (prop) {
-              elem[style] = (from+step*(to-from))+unit;
-          } else {
-              elem.style[style] = (from+step*(to-from))+unit;
-          }
-          if( step == 1) clearInterval(timer);
+        var step = Math.min(1,(new Date().getTime()-start)/time);
+        if (prop) {
+          elem[style] = (from+step*(to-from))+unit;
+        } else {
+          elem.style[style] = (from+step*(to-from))+unit;
+        }
+        if( step == 1) clearInterval(timer);
       },25);
   elem.style[style] = from+unit;
 }
@@ -19,8 +19,3 @@ var target = document.querySelector(".footer-box");
 el.addEventListener("click", function(){
   animate(document.body, "scrollTop", "", 0, target.offsetTop, 500, true);
 });
-
-// window.onload = function () {
-//   var target = document.getElementById("div5");
-//   animate(document.body, "scrollTop", "", 0, target.offsetTop, 2000, true);
-// };
